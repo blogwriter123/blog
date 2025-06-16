151ca036ad79c1df66478e4281657315787c5aee
blogwriter123 <blogwriter2025@gmail.com>
2025-06-16 16:02:03 +0530

    Update generatePost.js

--- generatePost.js
+++ generatePost.js
@@ -30,7 +30,7 @@ Rules:
       {
         model: 'gemini-1.5-flash',
         messages: [{ role: 'user', content: prompt }],
-        max_tokens: 1500,
+        max_tokens: 3000,
         temperature: 0.7,
       },
       {
